6cfc9db4f7ac2e86e0264c7ed93d1ac57d1b3d60
Andy <andy@Andys-MacBook-Pro.local>
2020-03-06 13:13:07 -0600

    adding db and auth

---
+++ firstAngularFirebaseIonic/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDSdmAECZedtlnud9BIVx3EQbR_A6NjXig",
+    authDomain: "firstangularfirebaseionic.firebaseapp.com",
+    databaseURL: "https://firstangularfirebaseionic.firebaseio.com",
+    projectId: "firstangularfirebaseionic",
+    storageBucket: "firstangularfirebaseionic.appspot.com",
+    messagingSenderId: "534095235829",
+    appId: "1:534095235829:web:6d0d549ff2b4945cddd3bc",
+    measurementId: "G-4LMB2WHQ7V"
+  }
+};
---
+++ firstAngularFirebaseIonic/src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDSdmAECZedtlnud9BIVx3EQbR_A6NjXig",
+    authDomain: "firstangularfirebaseionic.firebaseapp.com",
+    databaseURL: "https://firstangularfirebaseionic.firebaseio.com",
+    projectId: "firstangularfirebaseionic",
+    storageBucket: "firstangularfirebaseionic.appspot.com",
+    messagingSenderId: "534095235829",
+    appId: "1:534095235829:web:6d0d549ff2b4945cddd3bc",
+    measurementId: "G-4LMB2WHQ7V"
+  }
+};
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
